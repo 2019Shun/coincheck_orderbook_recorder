c011a0478a03c874da0f204d23dd018be8aea118
takase <taka.shun.2022@gmail.com>
2022-06-16 22:09:35 +0900

    [feat] 1秒単位で取得するように変更

--- script/recordCoincheckBtcOrderbook.py
+++ script/recordCoincheckBtcOrderbook.py
@@ -10,6 +10,8 @@ import datetime
 ORDERBOOK_URL = 'https://coincheck.com/api/order_books'
 TICKER_URL = 'https://coincheck.com/api/ticker'
 RATE_URL = 'https://coincheck.com/api/rate/btc_jpy'
+DB_NAME = 'btc_orderbook_table'
+DB2_NAME = 'btc_orderbook_table2'
 
 db_ipaddress = os.environ['DB_IPADDRESS']
 db_name = os.environ['DB_NAME']
@@ -35,11 +37,33 @@ def task(arg1, arg2):
         print(e)
         print(traceback.format_exc())
 
+def task2(arg1, arg2):
+    try:
+        # 情報の取得
+        btc_orderbook = requests.get(ORDERBOOK_URL).json()
+
+        # 文字列を数値へ変換
+        for ask_or_bid in ['asks', 'bids']:
+            for i in range(len(btc_orderbook[ask_or_bid])):
+                btc_orderbook[ask_or_bid][i] = [float(btc_orderbook[ask_or_bid][i][0]), float(btc_orderbook[ask_or_bid][i][1])]
+
+        #DBへの接続とデータ挿入
+        db = PostgreConnect(db_ipaddress, db_name, 'public', db_user, db_password)
+        db.execute('insert into ' + DB2_NAME + ' (orderbook, created_at) \
+            values (\'' + json.dumps(btc_orderbook) + '\', current_timestamp(3))')
+            
+    except Exception as e:
+        print()
+        print(datetime.datetime.now())
+        print()
+        print(e)
+        print()
+
 
 # スケジュール登録
-signal.signal(signal.SIGALRM, task)
-signal.setitimer(signal.ITIMER_REAL, 10, 10)
+signal.signal(signal.SIGALRM, task2)
+signal.setitimer(signal.ITIMER_REAL, 10, 1)
 
 # イベント実行
 while True:
-    sleep(1)
+    sleep(10)
--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,5 +1,6 @@
 -- 既存テーブルの削除
 DROP TABLE IF EXISTS btc_orderbook_table;
+DROP TABLE IF EXISTS btc_orderbook_table2;
 
 -- テーブル作成
 CREATE TABLE btc_orderbook_table (
@@ -7,6 +8,13 @@ CREATE TABLE btc_orderbook_table (
   orderbook jsonb not null,
   ticker jsonb,
   rate jsonb,
+  created_at timestamp,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE btc_orderbook_table2 (
+  id serial,
+  orderbook jsonb not null,
   created_at timestamp,
   PRIMARY KEY (id)
 );
